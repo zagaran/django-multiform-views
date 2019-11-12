bba10cfd7cc0f035e041bb8ba8ad3925e5de084a
Jake Klingensmith <jake@zagaran.com>
2019-11-12 15:55:41 -0500

    Add code, documentation, run black

--- setup.py
+++ setup.py
@@ -2,37 +2,37 @@ import os
 from setuptools import find_packages, setup
 
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
     README = readme.read()
 
 # Allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    author='Zagaran, Inc.',
-    author_email='info@zagaran.com',
+    author="Zagaran, Inc.",
+    author_email="info@zagaran.com",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development :: Testing',
-        'Topic :: Software Development :: Libraries',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Libraries",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     description="An expanded version of Django's FormViews to handle multiple forms with separate submission",
-    install_requires=['django'],
-    license='MIT',
+    install_requires=["django"],
+    license="MIT",
     long_description=README,
     long_description_content_type="text/markdown",
-    name='django-multiform-views',
+    name="django-multiform-views",
     packages=find_packages(),
-    url='https://github.com/zagaran/django-multiform-views',
-    version='0.1.0-alpha',
+    url="https://github.com/zagaran/django-multiform-views",
+    version="0.1.0-alpha",
 )
--- views/edit.py
+++ views/edit.py
@@ -1,108 +1,170 @@
+from django import forms
+from django.core.exceptions import ImproperlyConfigured
+from django.http import HttpResponseRedirect, HttpResponseForbidden
+from django.views.generic.base import ContextMixin, TemplateResponseMixin, View
+from django.views.generic.detail import SingleObjectMixin
 
 
 class FormsMixin(ContextMixin):
-    """ """
+    """ Provide a way to show and handle multiple forms in a request. """
+
     success_url = None
     success_urls = {}
     form_classes = {}
 
     def get_initial(self, form_key):
+        """
+        Return the initial data to use for a form on this view for a particular form key.
+            Implement <form_key>_initial or get_<form_key>_initial() to specify initial data for individual forms.
+        """
         initial_attr = f"{form_key}_initial"
         if hasattr(self, initial_attr):
             return getattr(self, initial_attr)
+
         initial_method = f"get_{form_key}_initial"
         if hasattr(self, initial_method):
             return getattr(self, initial_method)()
-        else:
-            return {}
 
-    def get_prefix(self, form_key):
-        if hasattr(self, f"{form_key}_prefix"):
-            return getattr(self, f"{form_key}_prefix")
-        elif hasattr(self, f"get_{form_key}_prefix"):
-            return getattr(self, f"get_{form_key}_prefix")()
-        else:
-            return None
+        return {}
 
     def get_form_classes(self):
+        """Return the form classes to use."""
         return self.form_classes
 
     def get_form_class(self, form_key):
+        """Return the form class to use for a particular form key."""
         return self.get_form_classes()[form_key]
 
     def get_form(self, form_key):
+        """Return an instance of the form to be used in this view for a particular form key."""
         form_class = self.get_form_class(form_key)
         kwargs = self.get_form_kwargs(form_key)
-        if hasattr(self, f"get_{form_key}_kwargs"):
-            kwargs.update(getattr(self, f"get_{form_key}_kwargs")())
+        kwargs_method = f"get_{form_key}_kwargs"
+        if hasattr(self, kwargs_method):
+            kwargs.update(getattr(self, kwargs_method)())
         return form_class(**kwargs)
 
     def get_form_kwargs(self, form_key):
+        """
+        Return the keyword arguments for instantiating the form for a particular form key.
+            Implement get_<form_key>_kwargs to add additional keyword arguments like instance or prefix.
+        """
         kwargs = {
-            'initial': {
-                '_key': form_key,
-                **self.get_initial(form_key)
-            },
-            'prefix': self.get_prefix(form_key)
+            "initial": {"_key": form_key, **self.get_initial(form_key)},
         }
-        if self.request.method in ['POST', 'PUT'] and self.request.POST['_key'] == form_key:
-            kwargs.update({
-                'data': self.request.POST,
-                'files': self.request.FILES,
-            })
+        if (
+            self.request.method in ["POST", "PUT"]
+            and self.request.POST["_key"] == form_key
+        ):
+            kwargs.update(
+                {"data": self.request.POST, "files": self.request.FILES,}
+            )
         return kwargs
 
     def get_success_url(self, form_key):
-        """Return the URL to redirect to after processing a valid form."""
-        if hasattr(self, f"get_{form_key}_success_url"):
-            return getattr(self, f"get_{form_key}_success_url")()
+        """
+        Return the URL to redirect to after processing the valid form for a particular form key.
+            Implement get_<form_key>_success_url or add form_key to specify for individual forms
+        """
+        success_url_method = f"get_{form_key}_success_url"
+        if hasattr(self, success_url_method):
+            return getattr(self, success_url_method)()
         elif form_key in self.success_urls:
             return str(self.success_urls[form_key])
         else:
             if not self.success_url:
-                raise ImproperlyConfigured("No URL to redirect to. Provide a success_url.")
+                raise ImproperlyConfigured(
+                    "No URL to redirect to. Provide a success_url."
+                )
             return str(self.success_url)  # success_url may be lazy
 
     def form_valid(self, form, form_key):
-        if hasattr(self, f"{form_key}_valid"):
-            return getattr(self, f"{form_key}_valid")(form)
+        """
+        If the form for a particular form key is valid, redirect to the supplied URL.
+            Implement <form_key>_valid(self, form) to specify behavior for a single form
+        """
+        form_valid_method = f"{form_key}_valid"
+        if hasattr(self, form_valid_method):
+            return getattr(self, form_valid_method)(form)
         else:
             return HttpResponseRedirect(self.get_success_url(form_key))
 
     def form_invalid(self, form, form_key):
-        if hasattr(self, f"{form_key}_invalid"):
-            return getattr(self, f"{form_key}_invalid")(form)
+        """
+        If the form for a particular form key is invalid, render the invalid form.
+            Implement <form_key>_invalid(self, form) to specify behavior for a single form
+        """
+        form_invalid_method = f"{form_key}_valid"
+        if hasattr(self, form_invalid_method):
+            return getattr(self, form_invalid_method)(form)
         else:
             return self.render_to_response(self.get_context_data(**{form_key: form}))
 
     def get_context_data(self, **kwargs):
+        """
+        Insert form classes into the current context using their key as their variable name.
+
+        Also attaches a hidden field to every form in form classes called _key which helps direct submission
+            to the appropriate form.
+        """
         form_context = {}
         for form_key, form_class in self.get_form_classes().items():
             if form_key not in kwargs:
                 form = self.get_form(form_key)
-                form.fields['_key'] = forms.CharField(widget=forms.HiddenInput())
+                form.fields["_key"] = forms.CharField(widget=forms.HiddenInput())
                 form_context[form_key] = form
             else:
-                kwargs[form_key].fields['_key'] = forms.CharField(widget=forms.HiddenInput())
+                kwargs[form_key].fields["_key"] = forms.CharField(
+                    widget=forms.HiddenInput()
+                )
         return super().get_context_data(**kwargs, **form_context)
 
 
-class ProcessFormsView(ProcessFormView):
+class ProcessFormsView(View):
+    """Render forms on GET and process a single form on POST."""
+
+    def get(self, request, *args, **kwargs):
+        """Handle GET requests: instantiate blank versions of the forms."""
+        return self.render_to_response(self.get_context_data())
+
     def post(self, request, *args, **kwargs):
-        if "_key" in request.POST:
+        """
+        Handle POST requests: instantiate a form instance with the passed
+        POST variables and then check if it's valid.
+
+        The form class is chosen based on the hidden form input _key, which is
+        attached with the context
+        """
+        try:
             form_key = request.POST["_key"]
             form = self.get_form(form_key)
             if form.is_valid():
                 return self.form_valid(form, form_key)
             else:
                 return self.form_invalid(form, form_key)
-        else:
-            return super().post(request, *args, **kwargs)
+        except KeyError:
+            return HttpResponseForbidden()
 
 
 class BaseFormsView(FormsMixin, ProcessFormsView):
-    """  """
+    """A base view for displaying forms."""
 
 
 class FormsView(TemplateResponseMixin, BaseFormsView):
-    """ """
+    """A view for displaying forms and rendering a template response."""
+
+
+class ObjectFormsView(SingleObjectMixin, FormsView):
+    """
+    A view for attaching a single object to a FormsView
+
+    This is useful if you want to include ModelForms in your form view.
+    """
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
